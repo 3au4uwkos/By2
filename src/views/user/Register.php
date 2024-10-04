4196bacbbb716aab488bc430604ce45eb65c1c97
Matvei Zaitsev <3au4uwkos@gmail.com>
2024-10-04 11:47:04 +0300

    u

--- src/views/user/Register.php
+++ src/views/user/Register.php
@@ -4,6 +4,9 @@
     $css = LinkParser::getLink("css");
     $js = LinkParser::getLink("js");
     $main = LinkParser::getLink("main");
+    $login = LinkParser::getLink("login");
+    $regImg = LinkParser::getLink("regImg");
+    $regBack = LinkParser::getLink("regBack");
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +18,65 @@
     <link href="<?php echo $css ?>" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="<?php echo $js ?>" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body>
-    <div class="container col-md-4 mx-auto">
-        <h1>Hello World!!!</h1>
+<body background="<?php echo $regBack?>">
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                <form class="mx-1 mx-md-4" name="form" method="post" action="">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="text" id="login" class="form-control" required/>
+                      <label class="form-label" for="login">Your Login</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="password" id="password" class="form-control" required />
+                      <label class="form-label" for="password">Password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                      <input type="password" id="password_repeat" class="form-control" required />
+                      <label class="form-label" for="password_repeat">Repeat your password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Submit"/>
+                  </div>
+
+                  <p>Already have an account? <a href="<?php echo $login?>">Login here</a>.</p>
+
+                </form>
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                <div class="container mx-auto me-auto">
+                    <img src="<?php echo $regImg?>"
+                        class="img-fluid rounded mx-auto me-auto" alt="Sample image">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
 </body>
 </html>
